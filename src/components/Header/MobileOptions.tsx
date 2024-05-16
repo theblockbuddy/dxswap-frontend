e6e464c20f62154a4a7045c50dcc16117603efd2
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2024-05-16 18:17:27 +0530

    fix:add all navlink

--- src/components/Header/MobileOptions.tsx
+++ src/components/Header/MobileOptions.tsx
@@ -1,14 +1,15 @@
-import React, { useRef } from 'react';
-import styled from 'styled-components';
-import { ApplicationModal } from '../../state/application/actions';
-import { useCloseModals, useModalOpen, useToggleMobileMenu } from '../../state/application/hooks';
-import { transparentize } from 'polished';
-import { useTranslation } from 'react-i18next';
-import { NavLink } from 'react-router-dom';
-import { Menu } from 'react-feather';
-import Modal from '../Modal';
-import { Box, Flex } from 'rebass';
-import { useOnClickOutside } from '../../hooks/useOnClickOutside';
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+import { ApplicationModal } from '../../state/application/actions'
+import { useCloseModals, useModalOpen, useToggleMobileMenu } from '../../state/application/hooks'
+import { darken, transparentize } from 'polished'
+import { useTranslation } from 'react-i18next'
+import { NavLink } from 'react-router-dom'
+import { Menu } from 'react-feather'
+import Modal from '../Modal'
+import { Box, Flex } from 'rebass'
+import { useOnClickOutside } from '../../hooks/useOnClickOutside'
+import { ExternalLink } from '../../theme'
 
 const StyledMenu = styled.div`
   margin-left: 0.5rem;
@@ -18,9 +19,9 @@ const StyledMenu = styled.div`
   position: relative;
   border: none;
   text-align: left;
-`;
+`
 
-const activeClassName = 'ACTIVE';
+const activeClassName = 'ACTIVE'
 
 const StyledNavLink = styled(NavLink).attrs({
   activeClassName
@@ -41,20 +42,37 @@ const StyledNavLink = styled(NavLink).attrs({
     font-weight: 600;
     color: ${({ theme }) => theme.white};
   }
-`;
+`
+const StyledExternalLink = styled(ExternalLink)`
+  ${({ theme }) => theme.flexRowNoWrap}
+  align-items: left;
+  border-radius: 3rem;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text5};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19.5px;
+  height: 36px;
 
+  :hover,
+  :focus {
+    color: ${({ theme }) => darken(0.1, theme.text1)};
+  }
+`
 const Wrapper = styled(Flex)`
   width: 100%;
   background: ${({ theme }) => transparentize(0.25, theme.bg2)};
-`;
+`
 
-export default function MobileOptions({ history }: { history: any; }) {
-  const node = useRef<HTMLDivElement>();
-  const open = useModalOpen(ApplicationModal.MOBILE);
-  const toggle = useToggleMobileMenu();
-  const closeModals = useCloseModals();
-  const { t } = useTranslation();
-  useOnClickOutside(node, open ? toggle : undefined);
+export default function MobileOptions({ history }: { history: any }) {
+  const node = useRef<HTMLDivElement>()
+  const open = useModalOpen(ApplicationModal.MOBILE)
+  const toggle = useToggleMobileMenu()
+  const closeModals = useCloseModals()
+  const { t } = useTranslation()
+  useOnClickOutside(node, open ? toggle : undefined)
 
   return (
     // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
@@ -72,7 +90,7 @@ export default function MobileOptions({ history }: { history: any; }) {
               {t('swap')}
             </StyledNavLink>
           </Box>
-          <Box>
+          {/* <Box>
             <StyledNavLink
               id={`pool-nav-link`}
               to={'/pool'}
@@ -86,24 +104,81 @@ export default function MobileOptions({ history }: { history: any; }) {
             >
               {t('pool')}
             </StyledNavLink>
-          </Box>
-          {/* <Box>
-            <StyledExternalLink id={`stake-nav-link`} href={'https://1hive.org/'}>
-              Governance <span style={{ fontSize: '11px' }}>↗</span>
-            </StyledExternalLink>
-          </Box>
-          <Box>
-            <StyledExternalLink id={`stake-nav-link`} href={'https://1hive.io/'}>
-              Farms <span style={{ fontSize: '11px' }}>↗</span>
-            </StyledExternalLink>
-          </Box>
-          <Box>
-            <StyledExternalLink id={`stake-nav-link`} href={'https://info.honeyswap.org/'}>
-              Charts <span style={{ fontSize: '11px' }}>↗</span>
-            </StyledExternalLink>
           </Box> */}
+
+          <Box>
+            <StyledExternalLink id={`stake-nav-link`} href={'https://espento.com/liquidity'}>
+              Liquidity
+              <span style={{ fontSize: '11px', marginTop: '3px', marginLeft: '3px' }}>
+                {' '}
+                <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="currentColor"
+                    d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                  ></path>
+                </svg>
+              </span>
+            </StyledExternalLink>
+          </Box>
+
+          <Box>
+            <StyledExternalLink id={`stake-nav-link`} href={'https://app.espento.network/'}>
+              Bridge{' '}
+              <span style={{ fontSize: '11px', marginTop: '3px', marginLeft: '3px' }}>
+                {' '}
+                <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="currentColor"
+                    d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                  ></path>
+                </svg>
+              </span>
+            </StyledExternalLink>
+          </Box>
+          <Box>
+            <StyledExternalLink id={`stake-nav-link`} href={'https://app.espento.network/stake'}>
+              Stake{' '}
+              <span style={{ fontSize: '11px', marginTop: '3px', marginLeft: '3px' }}>
+                {' '}
+                <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="currentColor"
+                    d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                  ></path>
+                </svg>
+              </span>
+            </StyledExternalLink>
+          </Box>
+          <Box>
+            <StyledExternalLink id={`stake-nav-link`} href={'https://www.espento.com/game'}>
+              Game{' '}
+              <span style={{ fontSize: '11px', marginTop: '3px', marginLeft: '3px' }}>
+                {' '}
+                <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="currentColor"
+                    d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                  ></path>
+                </svg>
+              </span>
+            </StyledExternalLink>
+          </Box>
+          <Box>
+            <StyledExternalLink id={`stake-nav-link`} href={'https://app.espento.network/affiliate'}>
+              Affiliate{' '}
+              <span style={{ fontSize: '11px', marginTop: '3px', marginLeft: '3px' }}>
+                {' '}
+                <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="currentColor"
+                    d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                  ></path>
+                </svg>
+              </span>
+            </StyledExternalLink>
+          </Box>
         </Wrapper>
       </Modal>
     </StyledMenu>
-  );
+  )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,8 +16,9 @@ import MobileOptions from './MobileOptions'
 import { useNativeCurrency } from '../../hooks/useNativeCurrency'
 
 import { useWeb3React } from '@web3-react/core'
-import { TYPE } from '../../theme'
+import { ExternalLink, TYPE } from '../../theme'
 import esptLogo from '../../assets/svg/esptsvg.svg'
+import { Text } from 'rebass'
 
 const HeaderFrame = styled.div`
   display: grid;
@@ -187,25 +188,25 @@ export const StyledNavLink = styled(NavLink).attrs({
   `};
 `
 
-// const StyledExternalLink = styled(ExternalLink).attrs({
-//   activeClassName
-// }) <{ isActive?: boolean; }>`
-//   ${({ theme }) => theme.flexRowNoWrap}
-//   align-items: left;
-//   outline: none;
-//   cursor: pointer;
-//   text-decoration: none;
-//   color: ${({ theme }) => theme.text5};
-//   font-weight: 400;
-//   font-size: 16px;
-//   line-height: 19.5px;
-//   width: fit-content;
-//   text-decoration: none !important;
-//   margin: 0 12px;
-//   ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-//     display: none;
-//   `};
-// `;
+const StyledExternalLink = styled(ExternalLink).attrs({
+  activeClassName
+})<{ isActive?: boolean }>`
+  ${({ theme }) => theme.flexRowNoWrap}
+  align-items: left;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text5};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19.5px;
+  width: fit-content;
+  text-decoration: none !important;
+  margin: 0 12px;
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    display: none;
+  `};
+`
 
 function Header({ history }: { history: any }) {
   const { account } = useActiveWeb3React()
@@ -236,7 +237,7 @@ function Header({ history }: { history: any }) {
           <StyledNavLink id={`swap-nav-link`} to={'/swap'} isActive={() => history.location.pathname.includes('/swap')}>
             {t('Swap')} <span style={styles}>BETA</span>
           </StyledNavLink>
-          <StyledNavLink
+          {/* <StyledNavLink
             id={`pool-nav-link`}
             to={'/pool'}
             isActive={() =>
@@ -247,25 +248,62 @@ function Header({ history }: { history: any }) {
             }
           >
             {t('pool')}
-          </StyledNavLink>
-          {/* <StyledExternalLink id={`stake-nav-link`} href={`https://1hive.org/`}>
-            Governance{' '}
-            <Text ml="4px" fontSize="11px">
-              ↗
+          </StyledNavLink> */}
+          <StyledExternalLink id={`stake-nav-link`} href={`https://espento.com/liquidity`}>
+            Liquidity
+            <Text ml="3px" fontSize="11px" marginTop="3px">
+              <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="currentColor"
+                  d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                ></path>
+              </svg>
             </Text>
           </StyledExternalLink>
-          <StyledExternalLink id={`stake-nav-link`} href={`https://1hive.io/`}>
-            Farms{' '}
-            <Text ml="4px" fontSize="11px">
-              ↗
+          <StyledExternalLink id={`stake-nav-link`} href={`https://app.espento.network/`}>
+            Bridge
+            <Text ml="3px" fontSize="11px" marginTop="3px">
+              <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="currentColor"
+                  d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                ></path>
+              </svg>
             </Text>
           </StyledExternalLink>
-          <StyledExternalLink id={`stake-nav-link`} href={`https://info.honeyswap.org/`}>
-            Charts{' '}
-            <Text ml="4px" fontSize="11px">
-              ↗
+          <StyledExternalLink id={`stake-nav-link`} href={`https://app.espento.network/stake`}>
+            Stake
+            <Text ml="3px" fontSize="11px" marginTop="3px">
+              <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="currentColor"
+                  d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                ></path>
+              </svg>
             </Text>
-          </StyledExternalLink> */}
+          </StyledExternalLink>
+          <StyledExternalLink id={`stake-nav-link`} href={`https://www.espento.com/game`}>
+            Game
+            <Text ml="3px" fontSize="11px" marginTop="3px">
+              <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="currentColor"
+                  d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                ></path>
+              </svg>
+            </Text>
+          </StyledExternalLink>{' '}
+          <StyledExternalLink id={`stake-nav-link`} href={`https://app.espento.network/affiliate`}>
+            Affiliate
+            <Text ml="3px" fontSize="11px" marginTop="3px">
+              <svg viewBox="0 0 24 24" color="primary" width="14px" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="currentColor"
+                  d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"
+                ></path>
+              </svg>
+            </Text>
+          </StyledExternalLink>{' '}
           <MobileSettingsWrap>
             <Settings />
           </MobileSettingsWrap>
